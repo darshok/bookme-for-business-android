7dfc7b14e5541e59fb8dd73f1266994ae0ff3a0b
Daniel Fenollar <dfo96@hotmail.com>
2024-03-09 19:12:56 +0100

    Feature/hilt dependencies (#3)
    * feature: add hilt dependencies
    
    * feature: add hilt dependencies

--- app/src/main/java/com/app/bookme/ui/compose/MainActivity.kt
+++ app/src/main/java/com/app/bookme/ui/compose/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.app.bookme
+package com.app.bookme.ui.compose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.app.bookme.ui.theme.BookmeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
