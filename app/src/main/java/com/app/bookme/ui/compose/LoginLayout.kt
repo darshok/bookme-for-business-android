c2edcb50e095e15cb24f03fadc57131c3590e8c9
Darshok <dfo96@hotmail.com>
2024-03-17 14:43:35 +0100

    feat: added forgot password button

--- app/src/main/java/com/app/bookme/ui/compose/LoginLayout.kt
+++ app/src/main/java/com/app/bookme/ui/compose/LoginLayout.kt
@@ -1,15 +1,18 @@
 package com.app.bookme.ui.compose
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -42,14 +45,24 @@ fun LoginLayout() {
                     text = stringResource(id = R.string.password_label)
                 )
             })
-            Button(onClick = { /*TODO*/ }, Modifier.padding(vertical = 16.dp)) {
+            TextButton(onClick = { /*TODO*/ }) {
+                Text(text = stringResource(id = R.string.forgot_password), color = MaterialTheme.colorScheme.primary)
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier.padding(vertical = 16.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+                )
+            ) {
                 Text(text = stringResource(id = R.string.login_button))
             }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun GreetingPreview() {
     BookmeTheme {
