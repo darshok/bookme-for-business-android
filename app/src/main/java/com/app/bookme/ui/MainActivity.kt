8916b01dc5f6bacb225467fdaa00c1fd39edfc02
Darshok <dfo96@hotmail.com>
2024-04-10 18:50:16 +0200

    feature: added color palette and updated theme

--- app/src/main/java/com/app/bookme/ui/MainActivity.kt
+++ app/src/main/java/com/app/bookme/ui/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.app.bookme.presentation
+package com.app.bookme.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.app.bookme.presentation.login.LoginLayout
-import com.app.bookme.presentation.theme.BookmeTheme
+import com.app.bookme.ui.login.LoginLayout
+import com.app.bookme.ui.theme.BookmeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
--- app/src/main/java/com/app/bookme/ui/theme/Type.kt
+++ app/src/main/java/com/app/bookme/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.app.bookme.presentation.theme
+package com.app.bookme.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
